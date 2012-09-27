d0980f99ee17965aca28ec0c9b3481d38f01f440
Gerrit Vos <gevo27@gmail.com>
2012-09-27 09:39:13 +0200

    Debugging Package Type module

--- sites/default/modules/hc_package_types/js/hc_package_types.js
+++ sites/default/modules/hc_package_types/js/hc_package_types.js
@@ -16,26 +16,33 @@
       _event_days = "";
   
       var productId = $('input[name=product_id]').val();
-  
-      $.getJSON(Drupal.settings.basePath + 'hc_package_types/get_dates/' + productId, {}, function(data, textStatus, jqXHR) {
-          _event_days = data;
-          $('.filled_in_calendar input').attr('disabled', false);
-      });
-      
-      $('.filled_in_calendar input').datepicker({     
-        dateFormat: 'mm.dd.yy',
-        firstDay: 1, // first day is Monday
 
-      
-        beforeShowDay: function(date) { 
-          var day = (date.getMonth()+1) + '.' + date.getDate() + '.' + date.getFullYear();
-          if (_event_days[day]) {
-            return [true];
-          } else {
-            return [false];
-          }
-        }
-      });
+			// HARDCODE FOR DEBUGGING!     
+      var currentPackage = $('.form-item-attributes-field-package-type > select option:selected').val();
+
+      if (productId && currentPackage == '8') {  
+	      $.getJSON(Drupal.settings.basePath + 'hc_package_types/get_dates/' + productId, {}, function(data, textStatus, jqXHR) {
+	          _event_days = data;
+	          $('.filled_in_calendar input').attr('disabled', false);
+	      });
+	      
+	      $('.filled_in_calendar input').datepicker({     
+	        dateFormat: 'mm.dd.yy',
+	        firstDay: 1, // first day is Monday
+	
+	      
+	        beforeShowDay: function(date) { 
+	          var day = (date.getMonth()+1) + '.' + date.getDate() + '.' + date.getFullYear();
+	          var output = day + ": " + _event_days[day];
+//	          alert(output);
+	          if (_event_days[day])  {
+	            return [true];
+	          } else {
+	            return [false];
+	          }
+	        }
+	      });
+	    }
     }
   };
 
